dd0eb5f9ea37e0cc10bf80a51ba67e88645e8f24
prashant-btkl <53873488+prashant-btkl@users.noreply.github.com>
2020-10-24 11:44:30 +0530

    Update form.php
    changed url "yogeshpingle.co.in"

--- copo/form.php
+++ copo/form.php
@@ -155,7 +155,7 @@
     <?php
 
       $h=getallheaders();
-        if($h["Referer"]=="http://localhost/copo/form.php"){
+        if($h["Referer"]=="http://yogeshpingle.co.in/copo/form.php"){
           echo <<<EXCERPT
           <div class="w3-panel w3-pale-red w3-display-container w3-border">
           <span onclick="this.parentElement.style.display='none'" class="w3-button w3-large w3-display-topright">×</span>
